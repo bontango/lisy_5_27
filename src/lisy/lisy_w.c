ce172d6b3dedd56756c91b6c7660b69d5f51bf69
bontango <ralf@flipperkeller.de>
2021-01-15 17:59:54 +0100

    added special solenoid handling (SS relais) for Road Kings

--- src/lisy/lisy_w.c
+++ src/lisy/lisy_w.c
@@ -156,8 +156,8 @@ unsigned char swMatrixLISY_W[9] = { 0,0,0,0,0,0,0,0,0 };
 
 //internal flag fo AC Relais, default 0 ->not present
 unsigned char lisy_has_AC_Relais = 0;
-//special rouines needed for RoadKings
-unsigned char lisy_is_RoadKings = 0;
+//internal flag fo SS Relais, default 0 ->not present ( RoadKings only)
+unsigned char lisy_has_SS_Relais = 0;
 
 
 //init SW portion of lisy_w
@@ -193,10 +193,8 @@ switch(lisymini_game.typeno)
 		lisy_has_AC_Relais = 0;	
 		break;
 	case LISYW_TYPE_SYS11RK: 			//Road Kings is first game with AC Relais
-          	//lisy_has_AC_Relais = 1;                       //but has different numbering then later SYS11A
-          	lisy_is_RoadKings = 1;
-		lisy_has_AC_Relais = 0; //RTH for first test
-		break;
+          	lisy_has_SS_Relais = 1;                 //but has different numbering then later SYS11A
+		break;					//it is Sol#12  and is called Solenoid Select Relais
 	case LISYW_TYPE_SYS11A: 
 		lisy_has_AC_Relais = 1;	
 		break;
@@ -939,7 +937,7 @@ if (ls80opt.bitv.freeplay == 1) //only if freeplay option is set
 void lisy_w_solenoid_handler( void )
 {
 
-int i,j,sol_no;
+int i,j,sol_no,real_sol;
 static UINT32 mysol=0;
 int mux_sol_active = 0;
 uint8_t action;
@@ -947,7 +945,9 @@ uint8_t action;
 //this is needed as we sometimes in pinmame have a ac select
 //activation too early (see also PROC code in s11.c)
 static uint8_t ac_want_to_change = 0; //1== action 0; 2==action 2
+static uint8_t ss_want_to_change = 0; //1== action 0; 2==action 2
 static uint8_t current_ac_state = 0;
+static uint8_t current_ss_state = 0;
 
 //did something changed?
 if ( mysol != coreGlobals.solenoids)
@@ -963,13 +963,46 @@ if ( mysol != coreGlobals.solenoids)
 	//sol number starts with 1
         sol_no = i+1;
 
-	//in case of Solenoid 14 (AC Relais)
+	//in case of Solenoid 14 (AC Relais) or Solenoid 12 (SS Relais)
         //check if one of the multiplexed sols are still active
-        // (RTH: TODO need another routine for Road Kings)
+
+	//Sol 12 version (Sys11 Roadkings)
+	if ( ( sol_no == 12) & (lisy_has_SS_Relais == 1) )
+	{
+	  //lets check if any of the muxed solenoids are active now
+	  //in pinmame these are 5,13,14,15 ( SS-relais 0) and 25..28 ( SS-relais 1)
+	  //for checking we have to substract 1 !
+	  if ( CHECK_BIT(coreGlobals.solenoids,4)) mux_sol_active++;
+	  for(j=12; j<=14; j++)  if ( CHECK_BIT(coreGlobals.solenoids,j)) mux_sol_active++;
+	  for(j=24; j<=27; j++)  if ( CHECK_BIT(coreGlobals.solenoids,j)) mux_sol_active++;
+          //no muxed solenoid active, activate ss relais
+	  if (mux_sol_active == 0) 
+	    {
+	      lisy_api_sol_ctrl(12,action);
+	      current_ss_state = action;
+	      if ( ls80dbg.bitv.coils )
+		{
+                  sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: SS-Relais changed to %d",action);
+                  lisy80_debug(debugbuf);
+	        }
+	    }
+	  else //we have active solenoids, ss relais needs to be delayd
+	   {
+	        ss_want_to_change = action + 1; //just remember for next round
+	        if ( ls80dbg.bitv.coils )
+		{
+                  sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: SS-Relais wants change to %d, DELAYED due %d active muxed Solenoids",mux_sol_active,action);
+                  lisy80_debug(debugbuf);
+	        }
+	   }
+	}//sol == 12 and SS_Relais present
+
+	//Sol 14 version (Sys11 after Roadkings)
 	if ( ( sol_no == 14) & (lisy_has_AC_Relais == 1) )
 	{
 	  //lets check if any of the muxed solenoids are active now
-	  //in pinmame these are 1..8 ( AC-relais 0) and 25..33 ( AC-relais 1)
+	  //in pinmame these are 1..8 ( AC-relais 0) and 25..32 ( AC-relais 1)
+	  //for checking we have to substract 1 !
 	  for(j=0; j<=7; j++)  if ( CHECK_BIT(coreGlobals.solenoids,j)) mux_sol_active++;
 	  for(j=24; j<=31; j++)  if ( CHECK_BIT(coreGlobals.solenoids,j)) mux_sol_active++;
           //no muxed solenoid active, activate ac relais
@@ -992,43 +1025,98 @@ if ( mysol != coreGlobals.solenoids)
                   lisy80_debug(debugbuf);
 	        }
 	   }
-	}//sol == 14	
+	}//sol == 14 and AC_Relais present
 
-        //for ac relais (sol 14) we have special routine (see above)
+        //for AC and SS relais (sol 14 and 12) we have special routine (see above)
 	//special solenoids, we with HW rules only by ignoring special switches 65 ... 70
 	//if the pinball (e.g. pinbot) is using special solenoids 'normal' we do it here
 	if (lisy_has_AC_Relais == 1)
 	  {
         	if ( ( sol_no != 14) &( sol_no <= 22 )) lisy_api_sol_ctrl(sol_no,action);
 	  }
+	else if (lisy_has_SS_Relais == 1)
+	  {
+        	if ( ( sol_no != 12) &( sol_no <= 22 )) lisy_api_sol_ctrl(sol_no,action);
+	  }
 	else
 	  {
-		//include sol 14 for systems without AC Relais
+		//include sol 14 and Sol 12  for systems without AC or SS Relais
         	if ( sol_no <= 22 ) lisy_api_sol_ctrl(sol_no,action);
 	  }
 
         //in case we hav solenoid #23, also activate #24 on APC
         //as APC use two solenoids for flipper (left/right)
         if (sol_no == 23 ) { lisy_api_sol_ctrl(23,action); lisy_api_sol_ctrl(24,action); }
+
 	//for games without AC Relais Williams use Sol 25 for flipper enable
         if ( (sol_no == 25 ) & (lisy_has_AC_Relais == 0)) { lisy_api_sol_ctrl(23,action); lisy_api_sol_ctrl(24,action); }
 
-	//with A-C Relais Solenoids 1..8 are muxed, in pinmame we have the 'C-Side' as Solenoids 25..33
+	//with A-C Relais Solenoids 1..8 are muxed, in pinmame we have the 'C-Side' as Solenoids 25..32
         //so we need to substract 24 before sending command to APC
 	if (( sol_no >=25) & (lisy_has_AC_Relais == 1)) { lisy_api_sol_ctrl(sol_no-24,action); }
 
-        //debug?
-        if ( ( ls80dbg.bitv.coils ) & ( sol_no != 14))
+	//with Solenoid Select (SS) Relais Solenoids 5,13,14,15 are muxed, in pinmame we have the 'C-Side' as Solenoids 25..28
+        //so we need to substract before sending command to APC
+	if ( lisy_has_SS_Relais == 1)
+	 { 
+                if (sol_no == 25 ) real_sol = 5;
+                else if (sol_no == 26 ) real_sol = 13;
+                else if (sol_no == 27 ) real_sol = 14;
+                else if (sol_no == 28 ) real_sol = 15;
+
+		lisy_api_sol_ctrl(real_sol,action);
+	 }
+
+        //debug? 
+        if ( ls80dbg.bitv.coils )
+	{
+        if ( ( sol_no != 14) & ( lisy_has_AC_Relais == 1))
         {
-	  if ( ( sol_no < 25)& (lisy_has_AC_Relais == 1))
+	  if ( sol_no < 25)
            { sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: Solenoid:%d, changed to %d ( AC is %d)",sol_no,action,current_ac_state); }
-	  else if ( ( sol_no >=  25)& (lisy_has_AC_Relais == 1))
-          { sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: Solenoid:%d(%d), changed to %d ( AC is %d)",sol_no-24,sol_no,action,current_ac_state); }
-          else
-          { sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: Solenoid:%d, changed to %d ( no AC Relais) ",sol_no,action); }
+	  else
+           { sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: Solenoid:%d(%d), changed to %d ( AC is %d)",sol_no-24,sol_no,action,current_ac_state); }
 
            lisy80_debug(debugbuf);
          }
+        else if ( ( sol_no != 12) & ( lisy_has_SS_Relais == 1))
+        {
+          if ( sol_no < 25)
+           { sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: Solenoid:%d, changed to %d ( SS is %d)",sol_no,action,current_ss_state); }
+          else
+           { sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: Solenoid:%d(%d), changed to %d ( SS is %d)",real_sol,sol_no,action,current_ss_state); }
+
+         }
+        else 
+           { 
+		if ( ( lisy_has_AC_Relais == 0) & ( lisy_has_SS_Relais == 0) )
+		sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: Solenoid:%d, changed to %d ( no AC or SS  Relais) ",sol_no,action); 
+	   }
+
+        lisy80_debug(debugbuf);
+
+	}
+
+	//do we have a delayd ss activation from last round?
+	if (ss_want_to_change != 0)
+	{
+	      //is it now save to activate?
+              if ( CHECK_BIT(coreGlobals.solenoids,4)) mux_sol_active++;
+              for(j=12; j<=14; j++)  if ( CHECK_BIT(coreGlobals.solenoids,j)) mux_sol_active++;
+              for(j=24; j<=27; j++)  if ( CHECK_BIT(coreGlobals.solenoids,j)) mux_sol_active++;
+	      if (mux_sol_active == 0)
+		{
+	          lisy_api_sol_ctrl(12,ss_want_to_change-1); 
+                  if ( ls80dbg.bitv.coils )
+                  {
+                    sprintf(debugbuf,"LISY_W_SOLENOID_HANDLER: SS-Relais DELAYD change to %d",ss_want_to_change-1);
+                    lisy80_debug(debugbuf);
+                  }
+		  current_ss_state = ss_want_to_change-1;
+		  ss_want_to_change = 0; //reset flag
+		}
+	 mux_sol_active = 0; //reset counter
+	}
 
 	//do we have a delayd ac activation from last round?
 	if (ac_want_to_change != 0)
@@ -1049,6 +1137,8 @@ if ( mysol != coreGlobals.solenoids)
 		}
 	 mux_sol_active = 0; //reset counter
 	}
+
+
       }//something changed
      }//for
    //store it for next call
